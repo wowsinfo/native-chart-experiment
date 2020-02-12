b3e4c03f001504f3e0574b5e0ca976d94f7cabb2
Yiheng <development.henryquan@gmail.com>
2020-02-12 23:25:09 +1100

    render a new chart everything 2 seconds

--- NativeChartExample/App.js
+++ NativeChartExample/App.js
@@ -16,44 +16,35 @@ import {
 } from 'react-native';
 import { NativeChart } from 'react-native-native-chart';
 
-const App = () => {
-  const { root, chart } = styles
-  return (
-    <SafeAreaView style={root}>
-      <View style={chart}>
-        <NativeChart style={chart} data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      </View>
-      <View style={chart}>
-        <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      </View>
-      <View style={chart}>
-        <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      </View>
-      <View style={chart}>
-        <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      </View>
-      <View style={chart}>
-        <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      </View>
-      <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      <NativeChart data={new Array([20.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0])}/>
-      <Text>What</Text>
-      <Text>What</Text>
-      <Text>What</Text>
-      <Text>What</Text>
-      <Text>What</Text>
-    </SafeAreaView>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [0.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0],
+    }
+
+    setInterval(() => {
+      this.setState({
+        data: Array.from({length: 10}, () => Math.floor(Math.random() * 50))
+      });
+    }, 2000);
+  }
+
+  render() {
+    const { root } = styles
+    return (
+      <SafeAreaView style={root}>
+        <NativeChart style={{height: 300, margin: 8}} chartData={this.state.data}/>
+      </SafeAreaView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-  },
-  chart: {
-    position: 'relative'
+    justifyContent: 'center',
   }
 });
 
