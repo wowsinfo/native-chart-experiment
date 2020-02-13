2d4aa32adbf5da1ab12ec35a4f13af49b04a71f6
Yiheng <development.henryquan@gmail.com>
2020-02-13 22:53:03 +1100

    import issue confirmed
    i will solve it tomorrow
    a bit too hard working recently

--- NativeChartExample/App.js
+++ NativeChartExample/App.js
@@ -10,11 +10,12 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Button,
   ScrollView,
-  Text
+  requireNativeComponent
 } from 'react-native';
-import { LineChart } from 'react-native-native-chart';
+import LineChart from 'react-native-native-chart';
+
+console.log(LineChart);
 
 class App extends React.Component {
   constructor(props) {
@@ -37,7 +38,6 @@ class App extends React.Component {
     return (
       <SafeAreaView style={root}>
         <ScrollView style={root} >
-          <Button ></Button>
           <LineChart style={{height: 300, margin: 8, backgroundColor: 'black'}} 
             chartData={[0.0, 10.0, 25.0, 15.0, 5.0, 30.0, 40.0, 10.0, 20.0, 45.0]} 
             legendLabel="White" darkMode themeColor="#FFFFFF"/>
