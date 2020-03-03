2d234711fcc3b33a4f8c3d7b7b6e002f6618612b
Yiheng <development.henryquan@gmail.com>
2020-03-03 13:56:18 +1100

    fixed export name

--- NativeChart/index.js
+++ NativeChart/index.js
@@ -1,3 +1,3 @@
 export { default as LineChart } from './src/LineChart';
 export { default as BarChart } from './src/BarChart';
-export { default as HorizontalBarChart } from './src/BarChart';
+export { default as HorizontalBarChart } from './src/HorizontalBarChart';
