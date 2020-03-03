97545156b6ccfc7d77636f33200682bb94a77674
Yiheng (NUC) <development.henryquan@gmail.com>
2020-03-03 23:19:30 +1100

    updated to kotlin

---
+++ NativeChart/android/src/main/java/com/reactlibrary/NativeChartPackage.kt
@@ -0,0 +1,24 @@
+package com.reactlibrary
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptModule
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+import java.util.*
+
+class NativeChartPackage : ReactPackage {
+    override fun createJSModules(): MutableList<Class<out JavaScriptModule>> {
+        return Collections.emptyList();
+    }
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return emptyList()
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return listOf<ViewManager<*, *>>(
+                NativeChartManager()
+        )
+    }
+}
---
+++ NativeChart/android/src/main/java/com/reactlibrary/NativeLineChartManager.kt
@@ -0,0 +1,25 @@
+package com.reactlibrary
+
+// AppCompatCheckBox import for React Native pre-0.60:
+// import android.support.v7.widget.AppCompatCheckBox;
+// AppCompatCheckBox import for React Native 0.60(+):
+import android.view.View
+import androidx.appcompat.widget.AppCompatCheckBox
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+
+class NativeLineChartManager : SimpleViewManager<View>() {
+    override fun getName(): String {
+        return REACT_CLASS
+    }
+
+    public override fun createViewInstance(c: ThemedReactContext): View {
+        val cb = AppCompatCheckBox(c)
+        cb.setChecked(true)
+        return cb
+    }
+
+    companion object {
+        const val REACT_CLASS = "NativeLineChart"
+    }
+}
