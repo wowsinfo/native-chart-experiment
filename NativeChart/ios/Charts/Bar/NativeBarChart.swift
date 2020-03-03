e32b499a51790653f6de4a5a336f63e9b3264ba6
Yiheng <development.henryquan@gmail.com>
2020-03-03 11:09:29 +1100

    started bar and pie

--- NativeChart/ios/Charts/Bar/NativeBarChart.swift
+++ NativeChart/ios/Charts/Bar/NativeBarChart.swift
@@ -7,3 +7,32 @@
 
 import Charts
 
+class NativeBarChart : BarChartView, OptimisedBarLineChart {
+    @objc var chartData: [Double] = []
+    // You will need one for each bar
+    @objc var legendLabel: [String] = []
+    @objc var darkMode: Bool = false
+    @objc var themeColor: String = "#FFFFFF"
+    
+    override func didSetProps(_ changedProps: [String]!) {
+        super.didSetProps(changedProps)
+        self.updateChart()
+    }
+    
+    init() {
+        super.init(frame: CGRect())
+        setupChart(chart: self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupChart(chart: BarLineChartViewBase) {
+        // TODO
+    }
+    
+    func updateChart() {
+        // TODO
+    }
+}
--- NativeChart/ios/Charts/Pie/NativePieChart.swift
+++ NativeChart/ios/Charts/Pie/NativePieChart.swift
@@ -6,3 +6,19 @@
 //
 
 import Charts
+
+class NativePieChart : PieChartView {
+    override func didSetProps(_ changedProps: [String]!) {
+        super.didSetProps(changedProps)
+//        self.updateChart()
+    }
+    
+    init() {
+        super.init(frame: CGRect())
+//        setupChart(chart: self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
